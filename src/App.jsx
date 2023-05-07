79bcbb45347471a4fa8a04973e106debe486b216
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-05-08 00:27:37 +0100

    Adding frontend mentor's template

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'styles.css';
+
+const App = () => {
+  return (
+    <div>
+        Features
+  Company
+  Careers
+  About
+
+  Login
+  Register
+
+  Make remote work
+
+  Get your team in sync, no matter your location. Streamline processes, 
+  create team rituals, and watch productivity soar.
+
+  Learn more
+    </div>
+  )
+}
+
+export default App
