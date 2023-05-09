3ddee64049531713ef8f148000bd25db7171eff3
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-05-09 23:45:19 +0100

    Adding mobile view and necessary interaction

---
+++ src/Components/Modal2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Modal2 = ({data}) => {
+  return (
+    <div id='Closing' className='closin' onClick={data}></div>
+  )
+}
+
+export default Modal2
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,10 +5,14 @@ import { Link } from "react-router-dom";
 import Company from './Company';
 import Features from './Features';
 import Modal from './Modal';
+import CloseNavM from '../images/icon-close-menu.svg';
+import OpenNavM from '../images/icon-menu.svg';
+import Modal2 from './Modal2';
 
 const Nav = () => {
     const [companyDrop, setCompanyDrop] = useState(false);
     const [featureDrop, setFeatureDrop] = useState(false);
+    const [mobileNav, setMobileNav] = useState(false);
 
     const flipDrop = () => {
         setCompanyDrop(!companyDrop);
@@ -51,35 +55,57 @@ const Nav = () => {
         setCompanyDrop(false);
         document.getElementById('CDown').parentNode.classList.remove('active');
     };
+
+    const mobileNavToggle = () => {
+        setMobileNav(!mobileNav);
+
+        if(mobileNav){
+            document.getElementById('Navigation').classList.remove('active-mobile-open');
+            document.getElementById('Closing').classList.remove('modall-active');
+            document.getElementById('Modal').classList.remove('modal-active');
+        } else {
+            document.getElementById('Navigation').classList.add('active-mobile-open');
+            document.getElementById('Closing').classList.add('modall-active');
+        }
+    }
     
   return (
     <>
-    <nav className='nav'>
-        <div className="nav-left">
-            <Link to='/home'>
-                <img src={Logo} alt="Logo" onClick={closeOpenNav} />
-            </Link>
-            <ul>
-                <li className='change' onClick={()=>{flipFDrop(); closeCOpenNav();}}>
-                    <Link to='/features' className='nav-color' >Features</Link>
-                    <img src={DropDown} alt="Arrow down" id='FDown' />
-                    <Features data={featureDrop}/>
-                </li>
-                <li className='change' onClick={()=>{flipDrop(); closeFOpenNav();}}>
-                    <Link to='/company' className='nav-color' >Company</Link>
-                    <img src={DropDown} alt="Arrow down" id='CDown' />
-                    <Company data={companyDrop} />
-                </li>
-                <li><Link to="/careers" className='nav-color' onClick={closeOpenNav}>Careers</Link></li>
-                <li><Link to="/about" className='nav-color' onClick={closeOpenNav}>About</Link></li>
-            </ul>
-        </div>
-        <div className="nav-right">
-            <Link to="/login" className='nav-color' onClick={closeOpenNav}>Login</Link>
-            <Link to="/register" className='nav-color color2' onClick={closeOpenNav}>Register</Link>
-        </div>
-    </nav>
+    <header className='header'>
+        <Link to='/home'>
+            <img src={Logo} alt="Logo" onClick={closeOpenNav} />
+        </Link>
+        <nav className='nav' id='Navigation'>
+            <img src={CloseNavM} id='MNClose' onClick={mobileNavToggle} alt="" />
+            <div className="nav-left">
+                <ul>
+                    <li className='change'>
+                        <div className="change-top" onClick={()=>{flipFDrop(); closeCOpenNav();}}>
+                            <Link to='/features' className='nav-color' >Features</Link>
+                            <img src={DropDown} alt="Arrow down" id='FDown' />
+                        </div>
+                        <Features data={featureDrop}/>
+                    </li>
+                    <li className='change'>
+                        <div className="change-top" onClick={()=>{flipDrop(); closeFOpenNav();}}>
+                            <Link to='/company' className='nav-color' >Company</Link>
+                            <img src={DropDown} alt="Arrow down" id='CDown' />
+                        </div>
+                        <Company data={companyDrop} />
+                    </li>
+                    <li><Link to="/careers" className='nav-color' onClick={closeOpenNav}>Careers</Link></li>
+                    <li><Link to="/about" className='nav-color' onClick={closeOpenNav}>About</Link></li>
+                </ul>
+            </div>
+            <div className="nav-right">
+                <Link to="/login" className='nav-color' onClick={closeOpenNav}>Login</Link>
+                <Link to="/register" className='nav-color color2' onClick={closeOpenNav}>Register</Link>
+            </div>
+        </nav>
+        <img src={OpenNavM} onClick={mobileNavToggle} alt="" />
+    </header>
     <Modal close={closeOpenNav}/>
+    <Modal2 data={mobileNavToggle} />
     </>
   )
 }
